de82bde49dd13e85c755f1961f9fa549279824c3
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2018-09-12 22:36:30 +0200

    styles for components added and recipes component filled with data

--- Recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is a test description', 'https://www.maxpixel.net/static/photo/1x/Bio-Food-Home-Made-Recipe-Dishes-Kitchen-Meals-1175496.jpg'),
+    new Recipe('Test recipe1', 'This is a test description1', 'https://www.maxpixel.net/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg'),
+    
+    new Recipe('Test recipe2', 'This is a test description2', 'https://www.maxpixel.net/static/photo/1x/Bio-Food-Home-Made-Recipe-Dishes-Kitchen-Meals-1175496.jpg')
+    
+  ];
 
   constructor() { }
 
